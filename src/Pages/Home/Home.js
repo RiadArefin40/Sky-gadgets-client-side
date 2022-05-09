0fba5a3fc8914715767a6df60bb9f70104a8a194
RiadArefin <arefinriad40@gmail.com>
2022-05-09 22:15:38 +0600

    Location added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from './Banner/Banner';
 import BestSelling from './BestSelling/BestSelling';
 import InventoryItems from './InventoryItems/InventoryItems';
+import Location from './Location/Location';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
              <Banner></Banner>
             <InventoryItems></InventoryItems>
             <BestSelling></BestSelling>
+            <Location></Location>
         </div>
     );
 };
---
+++ src/Pages/Home/Location/Location.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import './Location.css'
+
+const Location = () => {
+  return (
+    <div className="location">
+      <h2>Location to our warehouse</h2>
+      <div className=" d-flex ">
+      <div>
+          <h1>You Are Always invited to visit us</h1>
+        </div>
+        <div>
+          <img src="https://i.ibb.co/Kx813Rb/Capture.png" alt="" />
+        </div>
+        
+      </div>
+    </div>
+  );
+};
+
+export default Location;
