486f6b7bc140051411040e13d53861faa2a738fb
RiadArefin <arefinriad40@gmail.com>
2022-05-09 23:20:57 +0600

    fav icon added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -14,7 +14,7 @@ const Header = () => {
     <div>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand as={Link} to='/home'>React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand as={Link} to='/home'>Sky Gadgets</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
