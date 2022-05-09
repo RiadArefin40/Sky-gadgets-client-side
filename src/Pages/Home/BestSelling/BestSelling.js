954d13aef91d9514069b0a509a7bb1ea2a54c98d
RiadArefin <arefinriad40@gmail.com>
2022-05-09 21:22:32 +0600

    Best selling added

---
+++ src/Pages/Home/BestSelling/BestSelling.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BestSelling = () => {
+    return (
+        <div>
+            <h2>Our Best selling Products</h2>
+        </div>
+    );
+};
+
+export default BestSelling;
